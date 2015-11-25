bb567b8650857e696702290575ac79a411e5b7fa
Aliaxander <admin@oxcpa.ru>
2015-11-25 15:36:12 +0300

    Add api methods

--- libs/OxApi.php
+++ libs/OxApi.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Created by PhpStorm.
+ * Created by OxGroup.
  * User: Aliaxander
  * Date: 24.11.15
  * Time: 16:25
@@ -28,8 +28,76 @@ class OxApi
         $this->apiKey = $apiKey;
     }
 
-    protected function postApi($method, $request){
-
+    /**
+     * @param $method
+     * @param $request
+     *
+     * @return mixed|object
+     */
+    protected function requestApi($method, $request)
+    {
+        $curl = curl_init();
+        // Устанавливаем урл, к которому обратимся
+        curl_setopt($curl, CURLOPT_URL, $this->url . $method);
+        // максимальное время выполнения скрипта
+        curl_setopt($curl, CURLOPT_TIMEOUT, 20);
+        // передаем данные по методу post
+        curl_setopt($curl, CURLOPT_POST, 1);
+        // теперь curl вернет нам ответ, а не выведет
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        // переменные, которые будут переданные по методу post
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $request + array("email" => $this->email, "apikey" => $this->apiKey));
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        // отправка запроса
+        $result = curl_exec($curl);
+        // закрываем соединение
+        curl_close($curl);
+        if ($result) {
+            $result = json_decode($result, true);
+        } else {
+            $result = (object)array("result"=>array("error" => "Server is not responding"));
+        }
+        return $result;
     }
-    
+
+    /**
+     * @param $array
+     *
+     * @return mixed|object
+     */
+    public function addOrder($array)
+    {
+        return $this->requestApi("addOrder", $array);
+    }
+
+    /**
+     * @param $array
+     *
+     * @return mixed|object
+     */
+    public function getFlow($array)
+    {
+        return $this->requestApi("getFlow", $array);
+    }
+
+    /**
+     * @param $array
+     *
+     * @return mixed|object
+     */
+    public function addTraffic($array)
+    {
+        return $this->requestApi("addTraffic", $array);
+    }
+
+    /**
+     * @param $array
+     *
+     * @return mixed|object
+     */
+    public function getLandingConfig($array)
+    {
+        return $this->requestApi("getLandingConfig", $array);
+    }
+
 }
--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Created by PhpStorm.
+ * Created by OxGroup.
  * User: Aliaxander
  * Date: 25.11.15
  * Time: 14:35
@@ -9,4 +9,49 @@ use Ox\Api\OxApi;
 
 $loader = require __DIR__ . '/../vendor/autoload.php';
 
-$api=new OxApi();
+$api=new OxApi("http://api.justcpa.biz/","root@justcpa.biz","grtg345g5ujh67iu67iu");
+
+
+echo "<pre>";
+//Добавляем заказ:
+$params = array("user" => "43",//Только для API административного уровня
+    "ip" => "8.8.8.8",
+    "name" => "Имя",
+    "phone" => "5654445666",
+    "address" => "пушкина 23",
+    "counts" => "1",
+    "country" => "RU",//Страна оффера
+    "offer" => 19,//ID оффера
+    "subacc" => json_encode(array("sub1"=>"val1","sub2"=>"val2")),
+    "proxy" => "no",//Включен ли прокси у клиента
+    "orderId" => "345345345",//ID для отследивания
+    "hash" => "g566g65g5g",);//Уникальный код клиента
+print_r($api->addOrder($params));
+
+echo "<hr>";
+$params= array("user" => 0, //Только для API административного уровня
+    "flow" => 5,//ID потока
+    "offer" => 243//ID оффера
+);
+print_r($api->getFlow($params));
+//Вернет:
+//Array([result] => Array([id] => 5 [user] => 0 [name] => Тест 1 [options] => [offer] => 243 [promo] => 15 [metrika] => 0 [googleConversion] => 0 [topMail] => 0 [trafback] => [postbackApprove] => [postbackCancelled] => [postbackNew] => [transit] => 0 [date] => 2015 - 11 - 18 19:29:28 [split] => [on_disable] => [on_disable_url] => [add] => [advnet] => ) )
+echo "<hr>";
+//Добавляем посетителя. Уникальность определяется исходи их userhash, IP и оффера
+$params = array("ip" => "8.8.8.8",
+    "userhash" => "g566g65g5g",
+    "user" => 0,
+    "referer" => "http://google.com",
+    "offer" => 243,
+    "subaccs" => json_encode(array("sub1" => "val1", "sub2" => "val2"))
+);
+print_r($api->addTraffic($params));
+
+
+echo "<hr>";
+$params= array("hostName" => "test.slimliquid.ru",//Hostname лендинга в сети
+    "userHash" => "g566g65g5g"//Уникальный номер посетителя, для получения информации о нем
+);
+print_r($api->getLandingConfig($params));
+
+
